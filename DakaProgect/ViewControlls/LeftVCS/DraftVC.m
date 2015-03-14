b966ff171d8a05bc460b5d95df32dac663464f76
erxiao1 <251890812@qq.com>
2015-03-14 15:12:44 +0800

    左侧栏切换

---
+++ DakaProgect/ViewControlls/LeftVCS/DraftVC.m
@@ -0,0 +1,37 @@
+//
+//  DraftVC.m
+//  DakaProgect
+//
+//  Created by super on 15/3/14.
+//  Copyright (c) 2015年 lipeifeng. All rights reserved.
+//
+
+#import "DraftVC.h"
+
+@interface DraftVC ()
+
+@end
+
+@implementation DraftVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ DakaProgect/ViewControlls/LeftVCS/MyArtworkVC.h
@@ -0,0 +1,13 @@
+//
+//  MyArtworkVC.h
+//  DakaProgect
+//
+//  Created by super on 15/3/14.
+//  Copyright (c) 2015年 lipeifeng. All rights reserved.
+//
+
+#import "DKUIViewController.h"
+
+@interface MyArtworkVC : DKUIViewController
+
+@end
