b7987f1ce590276ea6e0e4347afe2e32348d3a05
erxiao1 <251890812@qq.com>
2015-04-06 18:49:31 +0800

    选择 分类 view 调整

--- DakaProgect/ViewControlls/UploadArtwork/ChooseSortVC.m
+++ DakaProgect/ViewControlls/UploadArtwork/ChooseSortVC.m
@@ -27,12 +27,12 @@
     [self.view setBackgroundColor:UIColorFromRGB(0xe5e5e5)];
      [UtilityFunc resetScrlView:_totalScrollView contentInsetWithNaviBar:YES tabBar:NO iOS7ContentInsetStatusBarHeight:0 inidcatorInsetStatusBarHeight:-1];
     _totalScrollView.backgroundColor = UIColorFromRGB(0xe5e5e5);
-    NSMutableArray *normalArray = [[NSMutableArray alloc] initWithObjects:@"youhua_normal",@"bingxi_nomal",@"banhua_nomal",@"shuicai_normal",@"shuimo_normal",@"shugao_normal",@"sumiao_normal",@"zonghe_normal",@"diaoshu_normal",@"yangsheng_normal",@"",@"",@"mingjian_normal",@"yuqi_normal",@"ciqi_normal",nil];
-    NSMutableArray *selectedArray = [[NSMutableArray alloc] initWithObjects:@"youhua_selected",@"bingxi_selected",@"banhua_selected",@"shuicai_selected",@"shuimo_selected",@"shugao_selected",@"sumiao_selected",@"zonghe_selected.png",@"diaoshu_selected",@"yangsheng_selected",@"",@"",@"mingjian_selected",@"yuqi_selected",@"ciqi_selected",nil];
+    NSMutableArray *normalArray = [[NSMutableArray alloc] initWithObjects:@"youhua_normal",@"bingxi_nomal",@"banhua_nomal",@"shuicai_normal",@"shuimo_normal",@"shugao_normal",@"sumiao_normal",@"zonghe_normal",@"diaoshu_normal",@"yangsheng_normal",@"yinxiang_normal",@"shexian_normal",@"mingjian_normal",@"yuqi_normal",@"ciqi_normal",nil];
+    NSMutableArray *selectedArray = [[NSMutableArray alloc] initWithObjects:@"youhua_selected",@"bingxi_selected",@"banhua_selected",@"shuicai_selected",@"shuimo_selected",@"shugao_selected",@"sumiao_selected",@"zonghe_selected.png",@"diaoshu_selected",@"yangsheng_selected",@"yinxiang_selected",@"shexian_selected",@"mingjian_selected",@"yuqi_selected",@"ciqi_selected",nil];
     
-//    NSMutableArray *nameArray = [[NSMutableArray alloc] initWithObjects:@"油画",@"丙烯",@"版画",@"水彩",@"水墨",@"书法手稿",@"素描手绘",@"综合材料",@"雕塑装置",@"艺术衍生品",@"",@"",@"民间美术",@"玉器",@"瓷器",nil];
+    NSMutableArray *nameArray = [[NSMutableArray alloc] initWithObjects:@"油画",@"丙烯",@"版画",@"水彩",@"水墨",@"书法手稿",@"素描手绘",@"综合材料",@"雕塑装置",@"艺术衍生品",@"影像",@"摄影",@"民间美术",@"玉器",@"瓷器",nil];
     
-    NSMutableArray *nameArray = [[NSMutableArray alloc] initWithObjects:@"版画",@"丙烯",@"瓷器",@"雕塑装置",@"民间美术",@"书法手稿",@"水彩",@"水墨",@"素描手绘",@"艺术衍生品",@"油画",@"玉器",@"杂项",@"综合材料",@"视频",nil];
+//    NSMutableArray *nameArray = [[NSMutableArray alloc] initWithObjects:@"版画",@"丙烯",@"瓷器",@"雕塑装置",@"民间美术",@"书法手稿",@"水彩",@"水墨",@"素描手绘",@"艺术衍生品",@"油画",@"玉器",@"杂项",@"综合材料",@"视频",nil];
     NSMutableArray *tagsArray = [[NSMutableArray alloc] initWithObjects:@"100",@"101",@"102",@"103",@"104",@"105",@"106",@"107",@"108",@"109",@"110",@"111",@"112",@"113",@"114", nil];
     CGRect rect = CGRectZero;
     rect.origin.x = ((iPhoneWidth) - 240)/4;
@@ -42,8 +42,11 @@
     for(int i = 0; i < [nameArray count];i++)
     {
         UIButton *button = [[UIButton alloc] init];
-        [button setTitle:[nameArray objectAtIndex:i] forState:UIControlStateNormal];
-        button.backgroundColor = [UIColor redColor];
+        [button setBackgroundImage:[UIImage imageNamed:[normalArray objectAtIndex:i]] forState:UIControlStateNormal];
+        [button setBackgroundImage:[UIImage imageNamed:[selectedArray objectAtIndex:i]] forState:UIControlStateHighlighted];
+        [button setBackgroundImage:[UIImage imageNamed:[selectedArray objectAtIndex:i]] forState:UIControlStateSelected];
+//        [button setTitle:[nameArray objectAtIndex:i] forState:UIControlStateNormal];
+//        button.backgroundColor = [UIColor redColor];
         button.tag = [[tagsArray objectAtIndex:i] intValue];
         button.frame = rect;
         [button addTarget:self action:@selector(sortButtonSelected:) forControlEvents:UIControlEventTouchUpInside];
@@ -54,6 +57,8 @@
             rect.origin.y += (rect.size.height+30);
         }
     }
+    
+    
     _totalScrollView.contentSize = CGSizeMake(0, CGRectGetMaxY(rect) +20);
 }
 
